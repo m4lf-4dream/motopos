480080f2a2dd776f0483351e5b1dfc015ad414c5
M4fs <roisalfian22@gmail.com>
2026-03-24 17:21:20 +0700

    session sessionan  2

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaksi extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'order_id',
+        'barang_id',
+        'jumlah',
+        'total_harga',
+        'metode_pembayaran',
+        'status'
+    ];
+
+    public function barang()
+{
+    return $this->belongsTo(Barang::class, 'barang_id');
+}
+
 }
